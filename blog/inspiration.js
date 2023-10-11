c2ed5ba3b6e21573ccb320e5982f97cce8c6130b
Justus-Kimutai <123356955+Justus-Kimutai@users.noreply.github.com>
2023-10-11 12:26:05 +0300

    Added the poster
    A description of the Hospital Visitation

--- blog/inspiration.js
+++ blog/inspiration.js
@@ -1,17 +1,19 @@
 let textDiv = document.querySelector('.text')
-const stringText = 'Lord You promised not to put me to shame, and indeed you are not a man to Lie, you have not ashemed me. Thank you God. Give me strength to always serve you all the days of my Life. '
+let img = document.querySelector('.img')
+
+const stringText = 'A Story is told of two men, Who were praying in a mountain. One was a millionaire praying for a breakthrough in a cetain contract. One was a poor guy crying for food to give his Children. The millionare Heard the poor guys graonings, He gave Him 1000 dollars. The poor guy went home with his answered prayer and sure enough God also opened a way for this millionare. You are a millonare today because God has given you a great health. There is a patient lying on the sick bed, some loosing hope of this life. Join us in prayer as we go and encourage them, pray for them and give them milk and Fruits. An God shall remmember the cries of your heart. '
 
 const arrString = stringText.split("")
 
-console.log(arrString)
 
 let index = 0
 
 let timeOut = setInterval(() => {
     textDiv.textContent += arrString[index]
     index++
+
     if(index === arrString.length){
         clearInterval(timeOut)
+        img.style.visibility = 'visible'
     }
 }, 100);
-
